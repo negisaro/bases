fcc818fbc51c2096730f1576775b0c0552f48ced
Giova Salazar <nesalaz56@gmail.com>
2024-09-08 13:05:21 -0500

    primer daño

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
-  templateUrl: './main-page.component.html'
-
+  templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 9500,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500
+    }
+  ];
 
+  onNewCharacter(charater: Character): void{
+    console.log(charater);
+  }
 }
