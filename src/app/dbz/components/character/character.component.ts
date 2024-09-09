2d0d5926a064d9f2aed5fed332dfeb4e6fdadd1c
Giova Salazar <nesalaz56@gmail.com>
2024-09-09 06:39:57 -0500

    docs creado

--- src/app/dbz/components/character/character.component.ts
+++ src/app/dbz/components/character/character.component.ts
@@ -4,27 +4,21 @@ import { Character } from '../../interfaces/character.interface';
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './character.component.html',
-  styleUrl: './character.component.css'
+  styleUrl: './character.component.css',
 })
 export class CharacterComponent {
-
   @Output()
   public onNewCharacter: EventEmitter<Character> = new EventEmitter();
 
   public character: Character = {
     name: '',
-    power: 0
-  }
+    power: 0,
+  };
 
-  emitCharacter(): void{
-
-    console.log(this.character)
-
-    if(this.character.name.length === 0) return;
+  emitCharacter(): void {
+    if (this.character.name.length === 0) return;
 
     this.onNewCharacter.emit(this.character);
-    this.character = {name: '', power: 0};
-
+    this.character = { name: '', power: 0 };
   }
-
 }
